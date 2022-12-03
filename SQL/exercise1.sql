c51d6583707aa1534095bb13ab2ea325916f788c
Tayuba <ayubamonnietahiru@gmail.com>
2022-12-04 00:39:28 +0100

    exercise1

---
+++ SQL/exercise1.sql
@@ -0,0 +1,6 @@
+SELECT title, release_year FROM moviesdb.movies  where studio="marvel studios";
+SELECT * FROM moviesdb.movies  where title like "%Avenge%";
+SELECT release_year FROM moviesdb.movies  where title like "%The Godfather%";
+SELECT distinct studio from moviesdb.movies  where industry="bollywood";
+
+
