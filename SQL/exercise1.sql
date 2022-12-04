d94760015faa4a6ff339c4ccfdfba8e8d2a19500
Tayuba <ayubamonnietahiru@gmail.com>
2022-12-04 01:43:39 +0100

    Text Query in SELECT, DISTINCT, LIKE

--- SQL/exercise1.sql
+++ SQL/exercise1.sql
@@ -2,5 +2,3 @@ SELECT title, release_year FROM moviesdb.movies  where studio="marvel studios";
 SELECT * FROM moviesdb.movies  where title like "%Avenge%";
 SELECT release_year FROM moviesdb.movies  where title like "%The Godfather%";
 SELECT distinct studio from moviesdb.movies  where industry="bollywood";
-
-
